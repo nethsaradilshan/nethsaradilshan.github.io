c43ab90781009b92ec1ba8e49a5cdefe9c0bb795
Nethsara Sadeepa Dilshan <42896890+nethsaradilshan@users.noreply.github.com>
2023-05-21 20:38:16 +0530

    Update function.js

--- js/function.js
+++ js/function.js
@@ -198,76 +198,76 @@
 	/* hide banner text when scroll end*/
 
 	/*auto line when scrolling start*/
-	let ticking = false;
-	let last_known_scroll_position = 0;
-	let updatePath = false;
+	// let ticking = false;
+	// let last_known_scroll_position = 0;
+	// let updatePath = false;
 	
-	const element = document.querySelector('svg');
-	const path = element.querySelector('path')
-	let totalLength = 0;
+	// const element = document.querySelector('svg');
+	// const path = element.querySelector('path')
+	// let totalLength = 0;
 	
-	initPath(path)
+	// initPath(path)
 	
-	function initPath(path){
-	   totalLength = path.getTotalLength();
-	   path.style.strokeDasharray = `${totalLength}`;
-	   path.style.strokeDashoffset = totalLength;
-	}
+	// function initPath(path){
+	//    totalLength = path.getTotalLength();
+	//    path.style.strokeDasharray = `${totalLength}`;
+	//    path.style.strokeDashoffset = totalLength;
+	// }
 	
 	
-	function handleEntries(entries) {
-	  console.log(entries)
-	   entries.forEach(entry => {
-		console.log(entry)
-		if(entry.isIntersecting) {
-		  console.log(entry.target)
-		}
+	// function handleEntries(entries) {
+	//   console.log(entries)
+	//    entries.forEach(entry => {
+	// 	console.log(entry)
+	// 	if(entry.isIntersecting) {
+	// 	  console.log(entry.target)
+	// 	}
 		
-	  })
-	}
+	//   })
+	// }
 	
-	let observer = new IntersectionObserver((entries, observer) => { 
-		entries.forEach(entry => {
-		if(entry.isIntersecting){
-		  console.log(entry);
-		  updatePath = true;
-		} else {
-		  updatePath = false;
-		}
-		});
-	  }, {rootMargin: "0px 0px 0px 0px"});
+	// let observer = new IntersectionObserver((entries, observer) => { 
+	// 	entries.forEach(entry => {
+	// 	if(entry.isIntersecting){
+	// 	  console.log(entry);
+	// 	  updatePath = true;
+	// 	} else {
+	// 	  updatePath = false;
+	// 	}
+	// 	});
+	//   }, {rootMargin: "0px 0px 0px 0px"});
 	
 	
-	  observer.observe(element);
+	//   observer.observe(element);
 	
-	function doSomething(scroll_pos) {
-	  if(!updatePath) {
-		return;
-	  }
-	  window.requestAnimationFrame(()=>{
-		const center = window.innerHeight / 2;
-		const boundaries = path.getBoundingClientRect();
-		const top = boundaries.top;
-		const height = boundaries.height;
-		const percentage = (center - top ) / height;
-		const drawLength = percentage > 0 ? totalLength * percentage : 0;
-		path.style.strokeDashoffset = drawLength < totalLength ? totalLength - drawLength : 0;
+	// function doSomething(scroll_pos) {
+	//   if(!updatePath) {
+	// 	return;
+	//   }
+	//   window.requestAnimationFrame(()=>{
+	// 	const center = window.innerHeight / 2;
+	// 	const boundaries = path.getBoundingClientRect();
+	// 	const top = boundaries.top;
+	// 	const height = boundaries.height;
+	// 	const percentage = (center - top ) / height;
+	// 	const drawLength = percentage > 0 ? totalLength * percentage : 0;
+	// 	path.style.strokeDashoffset = drawLength < totalLength ? totalLength - drawLength : 0;
 	 
-	  })
+	//   })
 	
-	}
+	// }
 	
-	window.addEventListener('scroll', function(e) {
-	  last_known_scroll_position = window.scrollY;
-	  if (!ticking) {
-		window.requestAnimationFrame(function() {
-		  doSomething(last_known_scroll_position);
-		  ticking = false;
-		});
+	// window.addEventListener('scroll', function(e) {
+	//   last_known_scroll_position = window.scrollY;
+	//   if (!ticking) {
+	// 	window.requestAnimationFrame(function() {
+	// 	  doSomething(last_known_scroll_position);
+	// 	  ticking = false;
+	// 	});
 	
-		ticking = true;
-	  }
-	});
+	// 	ticking = true;
+	//   }
+	// });
 	/*auto line when scrolling end*/
 
 
